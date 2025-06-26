a84d71f0391cac81035d95dd2df109e94b59ffe2
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-06-26 23:19:04 +0530

    modified some files

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Nav from "./Nav";
+import Banner from "./Banner";
 
 const Header = () => {
   return (
     <div>
-      <Nav />
+      <Banner />
     </div>
   );
 };
