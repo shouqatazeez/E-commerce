d5cd8e6bb59f0384a14a9cc6cd5e6afcb983628a
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-06-30 23:49:26 +0530

    add nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <div>
       <Banner />
+      <Nav />
     </div>
   );
 };
